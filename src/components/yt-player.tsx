603248bbca6bb5d225076876c97810594b94d4a1
Shounak Amladi <shounakamladi@cmail.carleton.ca>
2023-07-08 21:13:39 +0530

    adds visual when audio is playing.

--- src/components/yt-player.tsx
+++ src/components/yt-player.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
 import { useState } from 'react';
 import YouTube from 'react-youtube';
 export default function Player(props: { videoId: string, title: string }) {
     const [player, setPlayer] = useState<any>(null);
     const [answer, setAnswer] = useState<string>('');
     const [canShow, setCanShow] = useState<boolean>(false);
+    const [showPlayingGif, setShowPlayingGif] = useState<boolean>(false);
 
     const onReady = (event: any) => {
         setCanShow(false);
@@ -40,13 +42,27 @@ export default function Player(props: { videoId: string, title: string }) {
         if (canShow) return <h1 className='text-3xl pb-3'>{answer}</h1>
     };
 
+    const onStateChange = (event: any) => {
+        console.log(player)
+        if (player.getPlayerState() === 1) {
+            setShowPlayingGif(true);
+        } else {
+            setShowPlayingGif(false);
+        }
+    }
+
+    const renderPlayingGif = () => {
+        if (showPlayingGif) return <Image src='/audio-playing.gif' alt={'Audio is playing'} width='500' height='500'/>
+    }
+
     return (
         <div className='m-auto'>
-            <YouTube videoId={props.videoId} opts={opts} onReady={onReady} onPlay={onPlay} />
+            <YouTube videoId={props.videoId} opts={opts} onReady={onReady} onPlay={onPlay} onStateChange={onStateChange} />
             { renderAnswer() }
             <div className='flex flex-row justify-around'>
                 <button className='m-auto w-36 h-12 inline-block rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:text-black focus:outline-none focus:ring active:text-opacity-75' 
                     onClick={() => player.playVideo()}>Play Song</button>
+                { renderPlayingGif() }
                 <button className='m-auto w-36 h-12 inline-block rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:text-black focus:outline-none focus:ring active:text-opacity-75' 
                     onClick={() => showAnswer()}>Show Answer</button>
             </div>
