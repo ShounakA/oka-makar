0bc0e8f6b97e22a987f5c08253df7ade6636c0b9
Shounak Amladi <shounak.p.amladi@gmail.com>
2023-07-30 13:15:52 -0400

    feat: gets scoreboard via ws connection

--- src/pages/scorekeeper.tsx
+++ src/pages/scorekeeper.tsx
@@ -5,6 +5,7 @@ import { SpotifyPlayer, SpotifySession } from "@/utils/spotify_player";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
+import { Score } from "../../drizzle/schema";
 
 //@ts-ignore
 const fetcher = (args) => {
@@ -68,6 +69,33 @@ function DeviceSelector(props: {spotifyClient: SpotifyPlayer}) {
     }
 }
 
+function SessionScoreBoard(props: { session: number, passCode: string}) {
+    const [data, setData] = useState([] as Score[]);
+    const [ws, setWS] = useState<WebSocket>();
+    const [score, setScore] = useState<boolean>();
+    useEffect(() => {
+        const newWS = new WebSocket("ws://localhost:3001/ws/test")
+        newWS.onerror = err => console.error(err);
+        newWS.onopen = () => { 
+            setWS(newWS);
+            setScore(true);
+        }
+        newWS.onmessage = msg => setData(JSON.parse(msg.data));
+        return () => {
+            newWS.close();
+        }
+    }, [])
+    useEffect(() => {
+        if (score) {
+            ws?.send(JSON.stringify({ action: 'keeper', payload: { accessToken: "shounak", sessionId: props.session, passCode: props.passCode } }));
+            setScore(false);
+        }
+    }, [score]);
+    return (
+        <div> {data.map((d) => <div><span>{d.player}</span><span>{d.score}</span></div>)} </div>
+    )
+}
+
 function CreateSession(props: {scoreKeeperUserName?: string}) {
     const [passCode, setPassCode] = useState<string>('');
     const [session, setSession] = useState<number>(-1);
@@ -87,24 +115,30 @@ function CreateSession(props: {scoreKeeperUserName?: string}) {
     }
     if (session === -1) return (<SecondaryButton onClick={(_event) => postSession(props.scoreKeeperUserName)}><strong>Create a session</strong></SecondaryButton>)
     else return (
-        <div className="flex flex-row w-full justify-evenly align-middle items-center">
-            <div className='leading-9 text-center'>
+        <>
+            <div className="flex flex-row w-full justify-evenly align-middle items-center">
+                <div className='leading-9 text-center'>
+                    <div>
+                        GAME ROOM
+                    </div> 
+                    <span className='text-3xl'>
+                        #{session}
+                    </span>
+                </div>
                 <div>
-                    GAME ROOM
-                </div> 
-                <span className='text-3xl'>
-                    #{session}
-                </span>
+                    <div>
+                        Passcode
+                    </div>
+                    <span className='text-3xl'>
+                    {passCode}
+                    </span>
+                </div>
             </div>
             <div>
-                <div>
-                    Passcode
-                </div>
-                <span className='text-3xl'>
-                 {passCode}
-                </span>
+                {/* ADD PEOPLE JOINING THE GAME */}
+                <SessionScoreBoard session={session} passCode={passCode}/>
             </div>
-        </div>
+        </>
     )
 }
 
